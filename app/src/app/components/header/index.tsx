17ff1694e3a3e336880fc8466b5cf0189a919a56
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-02-08 23:28:24 -0300

    aula 04

---
+++ app/src/app/components/header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <div className="flex justify-between">
+        <span className="bg-black">Logo</span>
+      </div>
+    </header>
+  );
+}
---
+++ app/src/app/dash/messages/page.tsx
@@ -0,0 +1,25 @@
+export default function MessagesPage() {
+  return (
+    <>
+      <h1>sua caixa de entrada</h1>
+      <ul>
+        <li>
+          <a href="/app/dash/messages/1">
+            <span>mensagem 1</span>
+          </a>
+        </li>
+        <li>
+          <a href="/app/dash/messages/1">
+            <span>mensagem 2</span>
+          </a>
+        </li>
+        <li>
+          <a href="/app/dash/messages/1">
+            <span>mensagem 3</span>
+          </a>
+        </li>
+      </ul>
+      <button>voltar</button>
+    </>
+  );
+}
